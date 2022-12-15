f33b5d3dd7650d34c51f5b5bdb47ef5823c800b9
Jose <josemgomez40534@gmail.com>
2022-12-15 18:06:52 -0300

    subo act

--- ayuda/clase21/desafio3/routers/index.js
+++
@@ -1,5 +0,0 @@
-const { Router } = require("express");
-const router = Router()
-const userController = require('../controllers/user.controller')
-router.use('/users', userController)
-module.exports = router
--- ayuda/clase21/tdd/operaciones.js
+++
@@ -1,6 +0,0 @@
-const operacionesMatematicas = {
-    suma: (a, b) => a + b
-
-}
-
-module.exports = operacionesMatematicas
